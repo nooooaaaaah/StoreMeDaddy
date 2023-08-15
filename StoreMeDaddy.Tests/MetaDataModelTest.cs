b97b1cc45cfb2657dfaac9aabcc7f8102b2d71cc
Noah Spielman <nspielman@live.com>
2023-08-14 18:53:40 -0600

    more user stuff

--- StoreMeDaddy.Tests/MetaDataModelTest.cs
+++ StoreMeDaddy.Tests/MetaDataModelTest.cs
@@ -9,74 +9,87 @@ public class MetaDataModelTests
     [ExpectedException(typeof(ArgumentNullException))]
     public void TestNameCannotBeNull()
     {
-        _ = new MetaDataModel(null, "path", 100, "type", DateTime.Now, DateTime.Now, DateTime.Now, "user", "0123456789012345678901234567890123456789012345678901234567");
+        _ = new MetaDataModel(null, "", "path", 100, "type", true, false, null, "user", "0123456789012345678901234567890123456789012345678901234567");
     }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentNullException))]
     public void TestPathCannotBeNull()
     {
-        _ = new MetaDataModel("name", null, 100, "type", DateTime.Now, DateTime.Now, DateTime.Now, "user", "0123456789012345678901234567890123456789012345678901234567");
+        _ = new MetaDataModel("name", "", null, 100, "type", true, false, "1", "user", "0123456789012345678901234567890123456789012345678901234567");
     }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentNullException))]
     public void TestTypeCannotBeNull()
     {
-        _ = new MetaDataModel("name", "path", 100, null, DateTime.Now, DateTime.Now, DateTime.Now, "user", "0123456789012345678901234567890123456789012345678901234567");
+        _ = new MetaDataModel("name", "", "path", 100, null, true, false, "1", "user", "0123456789012345678901234567890123456789012345678901234567");
     }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentNullException))]
     public void TestCreatedByUserCannotBeNull()
     {
-        _ = new MetaDataModel("name", "path", 100, "type", DateTime.Now, DateTime.Now, DateTime.Now, null, "0123456789012345678901234567890123456789012345678901234567");
+        _ = new MetaDataModel("name", "", "path", 100, "type", true, false, "1", null, "0123456789012345678901234567890123456789012345678901234567");
     }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentNullException))]
     public void TestHashCannotBeNull()
     {
-        _ = new MetaDataModel("name", "path", 100, "type", DateTime.Now, DateTime.Now, DateTime.Now, "user", null);
+        _ = new MetaDataModel("name", "", "path", 100, "type", true, false, "1", "user", null);
     }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void TestInvalidSize()
     {
-        _ = new MetaDataModel("name", "path", -1, "type", DateTime.Now, DateTime.Now, DateTime.Now, "user", "0123456789012345678901234567890123456789012345678901234567");
+        _ = new MetaDataModel("name", "", "path", -1, "type", true, false, "1", "user", "0123456789012345678901234567890123456789012345678901234567");
     }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void TestInvalidHash()
     {
-        _ = new MetaDataModel("name", "path", 100, "type", DateTime.Now, DateTime.Now, DateTime.Now, "user", "invalid-hash");
+        _ = new MetaDataModel("name", "", "path", 100, "type", true, false, "1", "user", "invalid-hash");
+    }
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void TestInvalidVersion()
+    {
+        _ = new MetaDataModel("name", "", "path", 100, "type", true, false, null, "user", "0123456789012345678901234567890123456789012345678901234567");
     }
 
     [TestMethod]
     public void TestValidMetaData()
     {
-        Console.WriteLine("Testing Valid MetaData");
-        DateTime now = DateTime.Now;
+        // Console.WriteLine("Testing Valid MetaData");
         string name = "name";
+        string description = "";
         string path = "path";
         long size = 100L;
+        bool isPublic = true;
+        bool isDeleted = false;
+        string version = "1";
         string type = "type";
         string createdByUser = "user";
         string hash = "1111111111111111111111111111111111111111111111111111111111111111";
 
-        MetaDataModel metaData = new(name, path, size, type, now, now, now, createdByUser, hash);
-
-        Assert.AreEqual(name, metaData.Name);
+        MetaDataModel metaData = new(name, description, path, size, type, isPublic, isDeleted, version, createdByUser, hash);
+        DateTime timeCreated = metaData.CreatedAt;
+        Console.WriteLine($"Time created: {timeCreated}");
+        Assert.AreEqual(name, metaData.FileName);
         Assert.AreEqual(path, metaData.Path);
         Assert.AreEqual(size, metaData.Size);
         Assert.AreEqual(type, metaData.Type);
-        Assert.AreEqual(now, metaData.CreatedAt);
-        Assert.AreEqual(now, metaData.ModifiedAt);
-        Assert.AreEqual(now, metaData.AccessedAt);
+        Assert.AreEqual(isPublic, metaData.IsPublic);
+        Assert.AreEqual(isDeleted, metaData.IsDeleted);
+        Assert.AreEqual(version, metaData.Version);
+        Assert.AreEqual(createdByUser, metaData.CreatedByUser);
+        Assert.AreEqual(timeCreated, metaData.CreatedAt);
+        Assert.AreEqual(timeCreated, metaData.ModifiedAt);
+        Assert.AreEqual(timeCreated, metaData.AccessedAt);
         Assert.AreEqual(createdByUser, metaData.CreatedByUser);
         Assert.AreEqual(hash, metaData.Hash);
     }
 }
-
--- StoreMeDaddy/Models/MetaDataModel.cs
+++ StoreMeDaddy/Models/MetaDataModel.cs
@@ -1,52 +1,45 @@
 namespace StoreMeDaddy.Models;
 public class MetaDataModel
 {
-    public int Id { get; set;}
-    public string Name { get; set; }
+    public int Id { get; set; }
+    public string FileName { get; set; }
+    public string Description { get; set; }
     public string Path { get; set; }
     public long Size { get; set; }
     public string Type { get; set; }
+    public bool IsPublic { get; set; }
+    public bool IsDeleted { get; set; }
+    public string Version { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime ModifiedAt { get; set; }
     public DateTime AccessedAt { get; set; }
     public string CreatedByUser { get; set; }
     public string Hash { get; set; }
 
-    public MetaDataModel(string name, string path, long size, string type, DateTime createdAt, DateTime modifiedAt, DateTime accessedAt, string createdByUser, string hash)
+    public MetaDataModel(string FileName, string description, string path, long size, string type, bool isPublic, bool isDeleted, string? version, string createdByUser, string hash)
     {
-        if (string.IsNullOrEmpty(name))
-            throw new ArgumentNullException(nameof(name));
-        if (string.IsNullOrEmpty(path))
-            throw new ArgumentNullException(nameof(path));
-        if (string.IsNullOrEmpty(type))
-            throw new ArgumentNullException(nameof(type));
-        if (string.IsNullOrEmpty(createdByUser))
-            throw new ArgumentNullException(nameof(createdByUser));
-        if (createdAt > DateTime.Now || createdAt > modifiedAt || createdAt > accessedAt)
-            throw new ArgumentException("Invalid date", nameof(createdAt));
-        if (modifiedAt > DateTime.Now || modifiedAt > accessedAt)
-            throw new ArgumentException("Invalid date", nameof(modifiedAt));
-        if (accessedAt > DateTime.Now)
-            throw new ArgumentException("Invalid date", nameof(accessedAt));
-        if (size < 0)
-            throw new ArgumentException("Invalid size", nameof(size));
+
         if (string.IsNullOrEmpty(hash))
-            throw new ArgumentNullException(nameof(hash));
-        if (hash.Length != 64)
-            throw new ArgumentException("Invalid hash", nameof(hash));
-        Name = name;
-        Path = path;
-        Size = size;
-        Type = type;
-        CreatedAt = createdAt;
-        ModifiedAt = modifiedAt;
-        AccessedAt = accessedAt;
-        CreatedByUser = createdByUser;
-        Hash = hash;
+        {
+            throw new ArgumentNullException(nameof(hash), "Invalid hash");
+        }
+        this.FileName = FileName ?? throw new ArgumentNullException(nameof(FileName));
+        Description = description ?? "";
+        Path = path ?? throw new ArgumentNullException(nameof(path));
+        Size = size > 0 ? size : throw new ArgumentException("Invalid file size", nameof(size));
+        Type = type ?? throw new ArgumentNullException(nameof(type));
+        IsPublic = isPublic;
+        IsDeleted = isDeleted;
+        Version = version ?? "1";
+        CreatedAt = DateTime.Now;
+        ModifiedAt = CreatedAt;
+        AccessedAt = CreatedAt;
+        CreatedByUser = createdByUser ?? throw new ArgumentNullException(nameof(createdByUser));
+        Hash = hash.Length != 64 ? throw new ArgumentException("Invalid hash", nameof(hash)) : hash;
     }
     public override string ToString()
     {
-        return $"{Name},{Path},{Size},{Type},{CreatedAt},{ModifiedAt},{AccessedAt},{CreatedByUser},{Hash}";
+        return $"{FileName},{Description},{Path},{Size},{Type},{IsPublic},{IsDeleted},{Version},{CreatedAt},{ModifiedAt},{AccessedAt},{CreatedByUser},{Hash}";
     }
 }
 
