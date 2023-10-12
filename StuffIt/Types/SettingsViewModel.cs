84af33c296091db3c7dcc87e3dc5e6a59974e71d
Noah Spielman <nspielman@live.com>
2023-10-12 00:27:30 -0600

    settings view model

---
+++ StuffIt/Types/SettingsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StuffIt;
+
+public class SettingsViewModel
+{
+    [Range(1, int.MaxValue, ErrorMessage = "Max Items must be greater than 0.")]
+    public int MaxItems { get; set; }
+
+    // Add other properties and validation attributes as needed
+}
