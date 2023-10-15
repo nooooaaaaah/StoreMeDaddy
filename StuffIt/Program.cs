4388c2953245ea26172d588bc9e722e97dfbbf7a
Noah Spielman <nspielman@live.com>
2023-10-15 04:00:29 -0600

    we gotta graph

--- StuffIt/Program.cs
+++ StuffIt/Program.cs
@@ -1,24 +1,32 @@
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Blazorise;
+using Blazorise.Tailwind;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
+builder.Services
+    .AddBlazorise()
+    .AddTailwindProviders()
+    .AddFontAwesomeIcons();
 var app = builder.Build();
 
 // Set the environment to Development
 builder.Environment.EnvironmentName = "Development";
 Console.WriteLine($"Environment: {builder.Environment.EnvironmentName}");
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-};
+}
 
 app.UseHttpsRedirection();
 
